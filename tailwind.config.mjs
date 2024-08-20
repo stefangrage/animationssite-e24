9297c5f08e62362ec2a7acce0114973a75c2e78f
stefangrage <hej@stefangrage.dk>
2024-08-20 21:45:21 +0200

    opdaterer filnavne

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            keyframes: {
+                wiggle: {
+                  '0%, 100%': { transform: 'rotate(-3deg)' },
+                  '50%': { transform: 'rotate(3deg)' },
+                }
+              },
+              animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite',
+              }
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
